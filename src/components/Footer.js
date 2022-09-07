c2adad2cd623cc2bcec4af65d01c4a004d99e73b
saimdenizertunc <saimdenizertunc@gmail.com>
2022-09-07 18:07:22 +0300

    Create Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,42 @@
+import { Box, Link, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import React from "react";
 
+function Copyright() {
+  return (
+    <Typography
+      variant="caption"
+      color="text.secondary"
+      align="center"
+      component="p"
+    >
+      {"Copyright © "}
+
+      {new Date().getFullYear()}
+      {"."}
+      <Link color="inherit" href="https://github.com/saimdenizertunc">
+        Deniz Ertunç <GitHubIcon size="small" />
+      </Link>
+    </Typography>
+  );
+}
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <Container maxWidth="lg">
+        <Box
+          display="flex"
+          flexWrap="wrap"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Copyright />
+        </Box>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
