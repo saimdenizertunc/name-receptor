6322235ded1ca2dd8e1cd9c6ce1d9d08ced26d89
saimdenizertunc <saimdenizertunc@gmail.com>
2022-09-07 17:52:50 +0300

    Add theme & create workspace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { lightBlue } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 function App() {
-  return <div>Test</div>;
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: lightBlue[500],
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <div>HELLO</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
