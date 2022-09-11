ee1af729182b99e14156f7dc18cb6275afb2c695
saimdenizertunc <saimdenizertunc@gmail.com>
2022-09-11 15:53:23 +0300

    fix routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import MapTooltip from "./components/map/MapTooltip";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/map" element={<MapTooltip />} />
     </Routes>
   </BrowserRouter>
 );
